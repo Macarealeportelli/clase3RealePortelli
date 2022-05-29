117b9de48bf45fe7335f0142f2d1543b1d462266
Macarena Reale Portelli <68354897+Macarealeportelli@users.noreply.github.com>
2022-05-29 14:45:14 -0300

    practicando ciclos

---
+++ js/ciclos.js
@@ -0,0 +1,33 @@
+// Ejercicio1: Pedir número mediante prompt y sumarle otro número en cada repetición, realizando una salida por cada resultado
+
+// let numero = parseInt(prompt("Ingrese un número:"))
+// let limite = 5
+
+// for(let i=0; i < limite; i++){
+//    numero = numero + 4
+//     console.log(" El resultado es ", numero)
+// }
+
+// Ejercicio2: Pedir un texto mediante prompt, concatenar en cada repetición, realizando una salida por cada resultado, hasta que se ingresa "ESC"
+// debugger
+// let texto = prompt("Ingrese una palabra para crear texto:")
+// let contador = 0
+
+// while(texto != "ESC") {
+//   console.log(texto + " "+ contador++)
+//   texto = prompt("Ingrese otra palabra")
+// }
+
+//Ejercicio3: Pedir un numero por prompt, repetir la salida del mensaje "Hola", la cantidad de veces ingresadas
+
+// let limite = parseInt(prompt("Ingrese un número: "))
+
+// for(let i = null; i <= limite; i++){
+//     console.log("Hola")
+// }
+
+
+
+
+
+
